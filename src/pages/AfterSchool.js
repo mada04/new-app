6e43899a6c1334aa527f3143e8a8f6b073526b03
mada04 <madalina.comanescu@alten.com>
2024-09-18 16:21:58 +0300

    listdown

--- src/pages/AfterSchool.js
+++ src/pages/AfterSchool.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import af from '../images/af.gif'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 export default function AfterSchool() {
     const ps = ["transport de la scoala", "masa de pranz", "efectuarea temelor", "activitati recreative", "gustare", "personal de specialitate"]
     const pm = ["supravegherea",
@@ -9,11 +11,20 @@ export default function AfterSchool() {
         "gandire libera",
         "exercitii fizice zilnice",
         "dezvoltarea abilitatilor in cadrul cluburilor"]
+    const [list, setList] = useState(false)
+    const [listPm, setListPm] = useState(false)
 
+    const openList = () => {
+        setList(!list)
+    }
+
+    const openListPM = () => {
+        setListPm(!listPm)
+    }
     return (
         <div id="afterSchools">
             <section
-                className="px-10 w-full flex flex-col lg:flex-row py-20 
+                className="px-10 w-full flex flex-col lg:flex-row pt-20 
                    align-center bg-blue-600 bg-opacity-25   max-w-5xl mx-auto">
                 <div className="flex-1">
                     <img src={af}
@@ -43,28 +54,33 @@ export default function AfterSchool() {
                 <div className="flex-1 flex flex-col justify-center
                                 items-center gap-5 px-6">
 
-                    <p className='uppercase text-blue-600'>Program scurt <ExpandMoreIcon/></p>
-                    <ul className='list-disc'>
-                    {ps && ps.map((s, i) => {
-                        return (<li key={i}
-                        >{s}
-                        </li>)
-                    })}
-                    </ul>
-                   
+                    <p className='uppercase text-blue-600'>Program scurt {list === true ? <ExpandMoreIcon onClick={openList} /> : <ExpandLessIcon onClick={openList} />}</p>
+                    {list === true && <ul className='list-disc'>
+                        {ps && ps.map((s, i) => {
+                            return (<li key={i}
+                            >{s}
+                            </li>)
+                        })}
+                    </ul>}
+
+
                 </div>
                 <div className="flex-1 flex flex-col justify-center
                                 items-center gap-5 px-6">
 
-                    <p className='uppercase text-blue-600'>Program Mediu <ExpandMoreIcon/></p>
+                    <p className='uppercase text-blue-600'>Program Mediu {listPm === true ? <ExpandMoreIcon onClick={openListPM} /> : <ExpandLessIcon onClick={openListPM} />}</p>
+
+                    {listPm == true && 
                     <ul className='list-disc list-outside'>
-                    {pm && pm.map((s, i) => {
-                        return (<li key={i}
-                        >{s}
-                        </li>)
-                    })}
+                        {pm && pm.map((s, i) => {
+                            return (<li key={i}
+                            >{s}
+                            </li>)
+                        })}
                     </ul>
-                  
+                    }
+
+
                 </div>
             </section>
         </div>
